6ec7830c7d87573769313ca820dc76f70e6e948e
Suparno Saha <63898260+suparno24@users.noreply.github.com>
2020-06-13 12:08:51 +0530

    Add files via upload
    Gun.cs contains the script to be added to gun for raycast shoot
    Mouselook.cs to look around
    target.cs applied to the target to be shooted

---
+++ Target.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+	public float health  = 20f;
+
+	public void TakeDamage(float amount){
+		health -= amount;
+		if (health <= 0f) {
+			Die ();
+		}
+	}
+	void Die(){
+		Destroy (gameObject);
+	}
+}
